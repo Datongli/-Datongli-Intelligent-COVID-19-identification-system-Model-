64e85e6899d81a59e91a7cb7f86e3126c07b50fd
Datongli <ldt20020517@163.com>
2023-04-11 22:49:25 +0800

    23.4.11及以前使用的特征融合网络，没有按照声学所的更改

--- 模型和网络/Parallel_network.py
+++ 模型和网络/Parallel_network.py
@@ -4,6 +4,7 @@
 """
 import torch
 from torch import nn
+import numpy as np
 import torch.nn.functional as F
 import ResNet
 import Covnet_3
@@ -24,40 +25,46 @@ class parallel_net(nn.Module):
         # 属性分配
         self.dropout = dropout
 
-        self.softmax = nn.Softmax(dim=1)
-        # 全连接分类
-        self.fc_1 = nn.Linear(256*2, 128)
-        self.relu = nn.ReLU(inplace=True)
-        self.fc_2 = nn.Linear(128, 32)
-        self.fc_3 = nn.Linear(32, num_classes)
-
         # 部分一用于承接时频差分特性，暂定使用resnet网络，最后两个节点
-        self.part_1 = ResNet.resnet18(num_classes=256, include_top=True, dropout=self.dropout)
+        self.part_1 = ResNet.resnet18(num_classes=32, include_top=True, dropout=self.dropout)
         # self.part_1 = Covnet_3.Covnet(drop_1=0.2, drop_2=0.2)
         # 部分二用于承接对数梅尔倒谱图，暂定使用resnet网络，最后两个节点
-        # self.part_2 = ResNet.resnet34(num_classes=256*2, include_top=True, dropout=self.dropout)
-        self.part_2 = Covnet_3.Covnet(drop_1=0.2, drop_2=0.2)
-        self.part_2 = GhostNet.GhostNet(num_classes=256, dropout=0.2)
+        self.part_2 = ResNet.resnet18(num_classes=32, include_top=True, dropout=self.dropout)
+        # self.part_2 = Covnet_3.Covnet(drop_1=0.2, drop_2=0.2)
+        # self.part_2 = GhostNet.GhostNet(num_classes=256, dropout=0.2)
+        # self.bn = nn.BatchNorm1d(256*2)
+
+        self.softmax = nn.Softmax(dim=1)
+        # 全连接分类
+        self.fc_1 = nn.Linear(32*2, num_classes)
+        # self.relu = nn.ReLU(inplace=True)
+        # self.fc_2 = nn.Linear(128, 32)
+        # self.fc_3 = nn.Linear(128, num_classes)
+
+
+
 
     # 前向传播
     def forward(self, data_1, data_2):
         x_1 = self.part_1(data_1)
         x_2 = self.part_2(data_2)
-        x_1 = x_1.cpu()
-        x_2 = x_2.cpu()
-        x_1 = x_1.detach().numpy().tolist()
-        x_2 = x_2.detach().numpy().tolist()
-        for i in range(len(x_1)):
-            for j in range(len(x_2[0])):
-                x_1[i].append(x_2[i][j])
-        x = torch.tensor(x_1)
+        # x_1 = x_1.cpu()
+        # x_2 = x_2.cpu()
+        # x_1 = x_1.detach().numpy().tolist()
+        # x_2 = x_2.detach().numpy().tolist()
+        # for i in range(len(x_1)):
+        #     for j in range(len(x_2[0])):
+        #         x_1[i].append(x_2[i][j])
+        # x = torch.tensor(x_1)
+        x = torch.cat((x_1, x_2), dim=1)
         x = x.to(device)
+        # x = self.bn(x)
         # 全连接分类
         x = self.fc_1(x)
-        x = self.relu(x)
-        x = self.fc_2(x)
-        x = self.relu(x)
-        x = self.fc_3(x)
+        # x = self.relu(x)
+        # x = self.fc_2(x)
+        # x = self.relu(x)
+        # x = self.fc_3(x)
         # 是不是应该加一层softmax
         # x = self.softmax(x)
         return x
